86b6c255b007e6d23f5400a7a273a48cae01c5bd
Juand2602 <juand_sepulveda@hotmail.com>
2025-12-02 11:26:26 -0500

    add tallas para niños

--- frontend/src/pages/Inventory/EditProduct.jsx
+++ frontend/src/pages/Inventory/EditProduct.jsx
@@ -52,6 +52,21 @@ const EditProduct = () => {
 
   // Opciones de datos - ahora vienen del store
   const shoesSizes = [
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26", 
+    "27",
+    "28",
+    "29",
+    "30",
+    "31",
+    "32",
+    "33",
+    "34",
     "35",
     "36",
     "37",
